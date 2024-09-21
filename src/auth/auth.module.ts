b9c957ed0032bed9a1fc9836bda86703db3d5489
Satam Kundu <satamkundu67@gmail.com>
2024-09-21 13:14:09 +0530

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
     imports: [
-        JwtModule.registerAsync({
+        JwtModule.registerAsync({ //JWT Module globally initialisation
             global:true,
             imports: [ConfigModule],
             inject: [ConfigService],
